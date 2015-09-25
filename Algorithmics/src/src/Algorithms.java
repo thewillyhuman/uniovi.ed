bba58b39f9550b15939bca6586e7479d37b5154b
unknown <uo236856@EII-L-03-01.aulas.uniovi.es>
2015-09-25 19:21:24 +0200

    AfterClass_1

--- Algorithmics/src/src/Algorithms.java
+++ Algorithmics/src/src/Algorithms.java
@@ -45,4 +45,103 @@ public class Algorithms {
 			TestBench.doNothing(i);
 		}
 	}
+	
+	/**
+	 * Simulates a factorial non recursive algorithm.
+	 * @param n long, the number to calculate the factorial.
+	 * @return the factorial of n.
+	 */
+	public static long factorial(long n) {
+		long result = 1;
+        for (long i = 1; i <= n; i++)
+            result *= i;
+        return result;
+	}
+	
+	/**
+	 * Simulates a factorial recursive algorithm.
+	 * @param n
+	 * @return the factorial of n.
+	 */
+	public static long factorialRec (long n) {
+		if (n == 0)
+			return 1;
+		else
+			return n * factorialRec(n-1);
+	}
+	
+	/**
+	 * Simulates a pow of 2 algorithm in a iterative way. 2^n.
+	 * @param n the exponent of 2.
+	 * @return the result after the pow method executed.
+	 */
+	public static long pow(long n) {
+		long result = 1;
+		for(long i = 1; i<=n; i++)
+			result *= 2;
+		return result;
+	}
+	
+	/**
+	 * ENORMOUS COMPLEXITY
+	 * Simulates a pow of 2 algorithm in a recursive way. 2^n
+	 * @param n the exponent of 2.
+	 * @return the result after the pow method executed.
+	 */
+	public static long powRec1(long n)
+	{
+		if(n==0)
+			return 1;
+		else
+			return powRec1(n-1) + powRec1(n-1);
+	}
+	
+	/**
+	 * Simulates a pow of 2 algorithm in a recursive way. 2^n
+	 * @param n the exponent of 2.
+	 * @return the result after the pow method executed.
+	 */
+	public static long powRec2(long n)
+	{
+		if(n==0)
+			return 1;
+		else
+			return 2*powRec2(n-1);
+	}
+	
+	/**
+	 * Simulates a pow of 2 algorithm in a recursive way. 2^n
+	 * @param n the exponent of 2.
+	 * @return the result after the pow method executed.
+	 */
+	public static long powRec3(long n)
+	{
+		if(n==0) {
+			return 1;
+		} else {
+			if(n%2==0)
+				return powRec3(n/2)*powRec3(n/2);
+			else
+				return powRec3(n/2)*powRec3(n/2)*2;
+		}
+	}
+	
+	/**
+	 * Simulates a pow of 2 algorithm in a recursive way. 2^n
+	 * @param n the exponent of 2.
+	 * @return the result after the pow method executed.
+	 */
+	public static long powRec4(long n)
+	{
+		if(n==0) {
+			return 1;
+		} else {
+			long aux = powRec4(n/2);
+			aux = aux * aux;
+			if(n%2!=0)
+				return aux*2;
+			else
+				return aux;
+		}
+	}
 }
