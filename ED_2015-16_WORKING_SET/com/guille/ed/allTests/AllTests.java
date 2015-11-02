2259092690594c974c48d8194aeaba12a7f053cc
Guillermo Facundo Colunga <colunga91@gmail.com>
2015-11-02 15:41:49 +0100

    All tests package and class added to run all the alltests classes just from one site.

---
+++ ED_2015-16_WORKING_SET/com/guille/ed/allTests/AllTests.java
@@ -0,0 +1,12 @@
+package com.guille.ed.allTests;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({	com.guille.ed.algorithmics.test.AlgorithmicsTest.class, com.guille.ed.avlTrees.tests.AllAVLTests.class,
+				com.guille.ed.graphs.tests.AllGraphsTests.class	})
+public class AllTests {
+
+}
